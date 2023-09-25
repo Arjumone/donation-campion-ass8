7b25217706160558d5d8faf43a081c9bf6ce9e56
Arju Mone <121635205+Arjumone@users.noreply.github.com>
2023-09-25 13:32:23 +0600

    added data in local storage

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,9 +2,11 @@ import { useLoaderData } from "react-router-dom";
 import DonationCard from "../DonationCard/DonationCard";
 
 
+
 const Home = () => {
     const donations = useLoaderData()
     // console.log(donations);
+    // const [donations]= getDonation()
 
     return (
         <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-2 mt-12 max-w-6xl mx-auto">
